728b309f03eb0ab58d52f8729c1facff72634085
Jonathan Barsky <barskyjonathan96@gmail.com>
2022-07-06 22:46:08 -0300

    maquetacion de un HOC

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index';
+// import App from './App/index';
 // import patito from '../TodoCounter'  funcionaria ponerle cualquier nombre... y esto invita a errores
 import './index.css';
 
+function withWathever(){
+  return function ComponenteDeVerdad(props){
+    return <h2>Buenasss</h2>
+  }
+}
+
+function App(props) {
+  return ( 
+    <h1>{props.saludo}, {props.nombre}</h1>
+  )
+}
+
+function withSaludo(saludo){
+  return function WrappedComponentWithSaludo(WrappedComponent){
+    return function ComponenteDeVerdad(props){
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo('Wenasssss')(App);
+
+
 ReactDOM.render(
-    <App />,
+    <AppWithSaludo nombre="Juanita"/>,
   document.getElementById('root')
 );
-
